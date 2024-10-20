290ca570036d203667809b803e3e771d80c55bfa
twdly <taiwoodley@gmail.com>
2024-10-20 23:18:38 +1100

    Create enrolment type system for student subjects page

---
+++ DegreePlanner/Components/Pages/Student/Subjects.razor.cs
@@ -0,0 +1,22 @@
+﻿using DegreePlanner.Data;
+using DegreePlanner.ViewModels;
+
+namespace DegreePlanner.Components.Pages.Student
+{
+	public partial class Subjects
+    {
+        public List<SubjectViewModel> subjects = [];
+        public bool typeSelected = false;
+        public UserSubjectState enrolType;
+
+        public void SetEnrolType(UserSubjectState type)
+        {
+            enrolType = type;
+        }
+
+        public async void LoadSubjects()
+        {
+            typeSelected = true;
+        }
+    }
+}
