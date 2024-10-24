6ba2509a9c2dc38dc31cec97fbef69444c5f8e16
twdly <taiwoodley@gmail.com>
2024-10-24 15:08:36 +1100

    Add new subjects to sample database

--- DegreePlanner/Services/DatabaseResetService.cs
+++ DegreePlanner/Services/DatabaseResetService.cs
@@ -51,7 +51,33 @@ namespace DegreePlanner.Services
 				Name = "Business Requirements Modelling",
 				SubjectCode = 31269
 			};
-			List<Subject> subjects = [prog1, prog2, dotnet, linearAlgebra, probability, brm];
+			Subject citp = new()
+			{
+				Name = "Communication for IT Professionals",
+				SubjectCode = 31265
+			};
+			Subject webSys = new()
+			{
+				Name = "Web Systems",
+				SubjectCode = 31268
+			};
+			Subject dbFun = new()
+			{
+				Name = "Database Fundamentals",
+				SubjectCode = 31271
+			};
+			Subject ios = new()
+			{
+				Name = "Application Development in the iOS Environment",
+				SubjectCode = 41889,
+				Prerequisites = [prog1]
+			};
+			Subject math1 = new()
+			{
+				Name = "Mathematics 1",
+				SubjectCode = 33130,
+			};
+			List<Subject> subjects = [prog1, prog2, dotnet, linearAlgebra, probability, brm, citp, webSys, dbFun, ios, math1];
 
 			// Create majors containing these subjects
 			List<Major> majors = [];
@@ -59,7 +85,7 @@ namespace DegreePlanner.Services
 			{
 				Name = "Enterprise Software Development",
 				Credits = 48,
-				Subjects = [prog1, dotnet]
+				Subjects = [prog2, dotnet, ios]
 			});
 			majors.Add(new()
 			{
@@ -71,11 +97,11 @@ namespace DegreePlanner.Services
 			// Create degrees containing majors and subjects
 			Degree compsci = new()
 			{
-				Name = "Computer Science",
+				Name = "Bachelor of Computing Science",
 				CoreCredits = 48,
 				ElectiveCredits = 48,
 				Majors = [majors[0], majors[1]],
-				Subjects = [prog1]
+				Subjects = [prog1, citp, webSys, math1]
 			};
 			Degree bit = new()
 			{
@@ -83,7 +109,7 @@ namespace DegreePlanner.Services
 				CoreCredits = 48,
 				ElectiveCredits = 48,
 				Majors = [majors[0]],
-				Subjects = [brm]
+				Subjects = [brm, citp, webSys, dbFun, prog1]
 			};
 			List<Degree> degrees = [compsci, bit];
 
